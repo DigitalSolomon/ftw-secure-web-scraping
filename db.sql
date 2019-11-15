7bf2e86639b10fdfbaf189c5e486869f4404460f
Jason Townes French <jtfrench@gmail.com>
2019-11-16 07:56:07 +0800

    Updated db.sql

--- db.sql
+++ db.sql
@@ -1,11 +1,7 @@
 CREATE DATABASE scraperdb;
 USE scraperdb;
 ALTER DATABASE scraperdb CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-CREATE TABLE reviews (id BIGINT(7) NOT NULL AUTO_INCREMENT,
-reviewId VARCHAR(100), title VARCHAR(200), stars VARCHAR(100), reviewDate VARCHAR(50), content VARCHAR(10000),PRIMARY KEY(id));
-ALTER TABLE reviews CONVERT TO CHARACTER SET utf8mb4 COLLATE
+CREATE TABLE listings (id BIGINT(7) NOT NULL AUTO_INCREMENT,
+agency VARCHAR(100), region VARCHAR(100), position VARCHAR(100), plantilla VARCHAR(100), postingDate VARCHAR(100), closingDate VARCHAR(100),PRIMARY KEY(id));
+ALTER TABLE listings CONVERT TO CHARACTER SET utf8mb4 COLLATE
 utf8mb4_unicode_ci;
-ALTER TABLE reviews CHANGE title title VARCHAR(200) CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-ALTER TABLE reviews CHANGE content content VARCHAR(10000) CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
